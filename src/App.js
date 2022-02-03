a6754e3ef386591b809a04ec951b109950a74b9f
Ali Raza Dar <dar.ali1993@gmail.com>
2022-02-03 10:08:33 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React from 'react';
+import './App.css';
+import logo from './images/logomain.png'
+
 
 function App() {
   return (
-    <nav class="" id="">
-	  
-    <a class="navbar-brand" href="/">
-	    <img alt="Marketing, Branding and Communications Agency" class="responsive" 
-      src="" 
-      title="Marketing, Branding and Communications Agency" />
-    </a>
+    // <nav className='nav-bar' >
+	
+    // <img className='nav-bar-logo' src={logo} alt="Marketing, Branding and Communications Agency"/>
 
-    
-</nav>
+    // </nav>
+    <nav class="nav-bar" id="">
+	   <a class="nav-bar-logo" href="#">
+	      <img alt="Marketing, Branding and Communications Agency"  
+           src={logo} style={{height:'105px' , width:'auto' , float: 'left !important'}}  />
+      </a>
+
+
+
+
+   </nav>
   );
 }
 
