5c33b08e908341564ec7b37985c360c072a3446d
Ali Raza Dar <dar.ali1993@gmail.com>
2022-06-02 15:16:04 +0200

    footer update

--- src/footer.js
+++ src/footer.js
@@ -6,11 +6,11 @@ export default function Footer (props) {
     return (
     <div className='footer-section'>
          <Container >
-        <Row style={{ marginTop:'35px', marginBottom:'20px', position:'relative', textAlign:'center'}}>
+        <Row style={{ marginTop:'20px', marginBottom:'20px', position:'relative', textAlign:'center'}}>
             <Col  xs={12} md={12}>
                 <br />
               
-               <p style={{color:'#00A993', lineHeight:'34px'}}>
+               <p style={{color:'#00A993'}}>
                © 2022 BY THE RELEVANCE HOUSE. AG
             </p>
             </Col>
