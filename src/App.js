9ec64362790e4acf29af24a238ce9d3d107d23dd
Ali Raza Dar <dar.ali1993@gmail.com>
2022-06-09 17:43:15 +0200

    Fix for gh-page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 import './fonts/norwester.ttf'
-import HowPage from "./How"
-import WhatPage from "./What"
 import {Routes, Route} from "react-router-dom"
 import Navbar from './navbar'
 import Footer from './footer';
 import HomePage from './home';
+import HowPage from "./How"
+import WhatPage from "./What"
 
 function App() {
   return (
     <div>
         <Navbar/>
         <Routes>
-        <Route exact path='/' element={<HomePage/>} />
-        <Route exact path="/how" element={<HowPage/>} />
-        <Route exact path="/what" element={<WhatPage/>} />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/how" element={<HowPage/>} />
+        <Route path="/what" element={<WhatPage/>} />
         </Routes>
         <Footer/>
 </div>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './fonts/norwester.ttf';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
    
-   <BrowserRouter>
+   <HashRouter>
     <App />
-    </BrowserRouter>,
+    </HashRouter>,
  
  document.getElementById('root')
 );
