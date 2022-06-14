b50ae7b4049b524f050e0d9737e35ba4bb456ee7
Ali Raza Dar <dar.ali1993@gmail.com>
2022-06-14 17:58:22 +0200

    hero section update

--- src/hero-section.js
+++ src/hero-section.js
@@ -4,7 +4,8 @@ import {Container, Row, Col} from 'react-bootstrap'
 import heroimage from './images/Relevant.svg'
 import scroll from "./images/scrolldown.svg"
 import { NavHashLink } from 'react-router-hash-link'
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from 'react-router-hash-link'
+
 
 export default function Hero (props) {
     return (
@@ -21,7 +22,7 @@ export default function Hero (props) {
                <h4 id='heading-2' style={{color:'c6c6c6 !important', marginTop:'15px'}}>
                    HAS IMPACT
                </h4>
-                {/* <hr style={{background: '#00a993'}}/> */}
+               
                 <br />
                <p id='hero-line' style={{color:'white', lineHeight:'34px', borderLeft:'3px solid #00A993'}}>
                &nbsp;&nbsp;&nbsp;EVERY GOOD IDEA FITS ON A POST-IT. <br />
@@ -33,12 +34,11 @@ export default function Hero (props) {
                <NavHashLink smooth to="/#welcome-section" className='btn-green'>
                Are you ready to be relevant ?
                </NavHashLink>
-               {/* <a href='./how' className='btn-green'>
-               Do you want to know ?
-                </a> */}
+             
             </Col>
             <Col xs={6} md={6} style={{ textAlign :'right' }}>
                 <img src={heroimage} alt='Relevant' style={{width: '500px', height: '500px'}}/>
+              
             </Col>
         </Row>
         <Row style={{ textAlign:'center', marginBottom:'40px', marginTop:'10px' }}>
